01fa7b105e33db4c0f9f53a5f13965962a19ddd4
Mandar Shirke <quadcodes@gmail.com>
2015-08-24 19:22:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var mosca = require('mosca');
+
+var settings = {
+  port: process.env.PORT || 5000,
+  static: './',
+  persistence: mosca.persistence.Memory
+};
+
+var server = new mosca.Server(settings, function() {
+  console.log('Mosca server is up and running')
+});
+
+var message = {
+  topic: '/hello/world',
+  payload: 'abcde', // or a Buffer
+  qos: 0, // 0, 1, or 2
+  retain: false // or true
+};
+
+server.publish(message, function() {
+  console.log('done!');
+});
