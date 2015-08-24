36082bd0eb2877e01a60bc3efc19ecf165259891
Mandar Shirke <quadcodes@gmail.com>
2015-08-24 23:43:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-var mosca = require('mosca');
-   
-var moscaSettings = {
-port: 1883,
- http: {
-     port: 5000,
-     bundle: true,
-     static: './'
-   }
- };
- 
- var server = new mosca.Server(moscaSettings);
- server.on('ready', setup);
- 
- server.on('clientConnected', function(client) {
-     console.log('client connected', client.id);     
- });
- 
- // fired when a message is received
- server.on('published', function(packet, client) {
-   console.log('Published', packet.payload);
- });
- 
- // fired when the mqtt server is ready
- function setup() {
-   console.log('Mosca server is up and running')
- }
+var mosca = require('mosca')
+
+//1883
+var settings = {
+    port: 3000
+};
+
+//here we start mosca
+var server = new mosca.Server(settings);
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
+}
+
+// fired whena  client is connected
+server.on('clientConnected', function(client) {
+  console.log('client connected', client.id);
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published MIO: ', packet.payload);
+});
+
+// fired when a client subscribes to a topic
+server.on('subscribed', function(topic, client) {
+  console.log('subscribed : ', topic);
+});
+
+// fired when a client subscribes to a topic
+server.on('unsubscribed', function(topic, client) {
+  console.log('unsubscribed : ', topic);
+});
+
+// fired when a client is disconnecting
+server.on('clientDisconnecting', function(client) {
+  console.log('clientDisconnecting : ', client.id);
+});
+
+// fired when a client is disconnected
+server.on('clientDisconnected', function(client) {
+  console.log('clientDisconnected : ', client.id);
+});
